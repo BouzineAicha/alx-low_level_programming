5519d9bd75819b14c846b3ca2e9dcd992ae59fd7
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-16 20:11:04 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,6 @@
 * Return: Always 0 (Success)
 */
 int main(void)
-{
-	printf("Programming is like building a multilingual puzzle");
+printf ("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
