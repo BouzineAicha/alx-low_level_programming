e8500164d06f8507ff2e5323ee9636624a601a87
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-20 14:19:35 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = a; c <= z; c++)
+putchar(c);
+putchar(n);
+return (0);
+}
