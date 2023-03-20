8c990df3538b1a79b54e3c2151d760474feebc15
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-20 14:42:50 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry function
+ *
+ * Return: 0
+ */
+int main(void)
+{
+char c;
+
+for (c = a; c <= z; c++)
+{
+if (c == q || c == e)
+continue;
+putchar(c);
+}
+putchar(n);
+return (0);
+}
