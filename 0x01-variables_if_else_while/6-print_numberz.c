54a012be9a7178b022d0bd804009059275ce3dbc
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-20 15:02:02 -0700

    task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,12 +7,12 @@
 
 int main(void)
 {
-int num;
+	int num;
 
-for (num = 0; num < 10; num++)
-{
-putchar(num + 48);
-}
-putchar(n);
-return (0);
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num + 48);
+	}
+	putchar('\n');
+	return (0);
 }
