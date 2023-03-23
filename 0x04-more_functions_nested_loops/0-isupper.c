ee1bfe8d08e4126fb1da6e2d9fcc9b67e4ddcb9f
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-23 08:56:18 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,5 +1,11 @@
 #include "main.h"
 #include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     char c;
@@ -10,3 +16,4 @@ int main(void)
     printf("%c: %d\n", c, _isupper(c));
     return (0);
 }
+
