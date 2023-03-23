e39bcc5aa018ccabd794f2efd3967fc6531d162b
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-23 16:26:09 -0700

    task

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,17 @@
+
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code.
+ * _isupper - uppercase letters
+ * @c: char to check
  *
- * Return: Always 0.
+ * Return: 0 or 1
  */
-int main(void)
-{
-    char c;
 
-    c = 'A';
-    printf("%c: %d\n", c, _isupper(c));
-    c = 'a';
-    printf("%c: %d\n", c, _isupper(c));
-    return (0);
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
