db66363101262bf9b18837197c6bfa4a1a2a396e
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-24 18:11:34 -0700

    task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+   * print_alphabet - order from a to z
+    *
+     *Return: x
+      */
+
+void print_alphabet(void)
+{
+	int x;
+
+	for (x = 97; x <= 122; x++)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+}
