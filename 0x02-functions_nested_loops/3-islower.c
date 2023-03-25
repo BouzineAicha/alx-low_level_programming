573012f2da5e20b3665499dc435cf621ece06733
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-03-24 18:20:39 -0700

    task 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+   *_islower - check if int c is lower
+    *
+     * @c: int checked
+      * Return: 1 if lower,0 if upper
+       */
+
+int _islower(int c)
+{
+	if (c  >= 97  &&  c  <=  122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putchar('\n');
+}
