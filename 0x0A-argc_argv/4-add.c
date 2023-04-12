eeb319af775396b69b30b6e041e129335f469f95
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-04-11 18:43:14 -0700

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include "holberton.h"
+
+/**
+ * main - Program that takes in all integer arguments and returns the sum
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if a non-integer is among the passed in arguments, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b, length, sum;
+	char *ptr;
+
+	if (argc < 2)
+		printf("0\n");
+	else
+	{
+		sum = 0;
+		for (a = 1; a < argc; a++)
+		{
+			ptr = argv[a];
+			length = strlen(ptr);
+
+			for (b = 0; b < length; b++)
+			{
+				if (isdigit(*(ptr + b)) == 0)
+				{
+					printf("Error\n");
+					return (1);
+				}
+			}
+
+			sum += atoi(argv[a]);
+		}
+
+	printf("%d\n", sum);
+	}
+	return (0);
+}
