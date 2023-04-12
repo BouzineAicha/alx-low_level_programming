44052b24f95d84926277f2f7ed7f6a97cc8b5762
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-04-11 19:21:54 -0700

    0x0B. C - malloc, free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *_strdup(char *str)
+{
+  int a = 0, b, c = 0;
+  char *newStr = NULL;
+
+  while (str[c] != '\0')
+    {
+      a++;
+      c++;
+    }
+
+  newStr = malloc(sizeof(char) * (a + 1));
+
+  if (str == NULL)
+    return (NULL);
+
+  for (b = 0; j <= a; b++)
+    newStr[b] = str[b];
+
+  return (newStr);
+}
