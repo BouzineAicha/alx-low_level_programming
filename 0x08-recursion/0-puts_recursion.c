b912cf35801153be7cddc6753b67291a2e6017d7
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-06-07 18:19:54 -0700

    task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * Writing a function that prints a string, followed by a new line.
+
+Using  the Prototype: void _puts_recursion(char *s);
+
+**/
+void _puts_recursion(char *s){
+	if(*s == 0){
+		_putchar('/n);
+		return;
+		   }
+	_putchar(*s);
+	_puts_recursion(s+1);
+	                     }
