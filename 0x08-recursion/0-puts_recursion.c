838d18e45673796a255439153adc36e0de22a3bd
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-06-07 18:29:44 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,11 +5,13 @@
 Using  the Prototype: void _puts_recursion(char *s);
 
 **/
-void _puts_recursion(char *s){
-	if(*s == 0){
+void _puts_recursion(char *s)
+{
+	if(*s == 0)
+	{
 		_putchar('\n');
 		return;
-		   }
+       	}
 	_putchar(*s);
 	_puts_recursion(s+1);
-	                     }
+}
