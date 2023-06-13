e69c6ed2e2b08595e838316e8aa3163faa5c8661
BouzineAicha <bouzine.aicha.solicode@gmail.com>
2023-06-12 17:03:42 -0700

    program that multiplies two number

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Program that takes first two integer arguments and prints the product
+ * @argc: Number of command line arguments
+ * @argv: Array name
+ * Return: 1 if not enough arguments passed in, 0 otherwise
+ */
+
+int main(int argc, char *argv[])
+{
+	int a, b;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		b = 1;
+
+		for (a = 1; a < 3; a++)
+			b *= atoi(argv[a]);
+
+		printf("%d\n", b);
+	}
+
+	return (0);
+}
